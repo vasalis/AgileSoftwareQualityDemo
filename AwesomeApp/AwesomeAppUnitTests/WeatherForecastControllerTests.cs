269485ee575dd77f984248772a1ba37455a09e95
Vassilis Salis <vsalis@outlook.com>
2020-11-04 11:40:50 +0200

    added first test

---
+++ AwesomeApp/AwesomeAppUnitTests/WeatherForecastControllerTests.cs
@@ -0,0 +1,24 @@
+using AwesomeApp.Controllers;
+using NUnit.Framework;
+using System.Linq;
+
+namespace AwesomeAppUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestGet()
+        {
+            WeatherForecastController lController = new WeatherForecastController(null);
+
+            var lResult = lController.Get();
+
+            Assert.AreEqual(lResult.ToList().Count, 5);            
+        }
+    }
+}
